1dacdbafd8c8886dd576c192aec1716a5f144d64
Tanner Campbell <tanner.campbell@rewst.io>
2024-05-19 21:06:05 +0100

    Added project 1 starter files

---
+++ number-guessing-game-v1/guessing_game.py
@@ -0,0 +1,27 @@
+"""
+Python Development Techdegree
+Project 1 - The Number Guessing Game
+--------------------------------
+"""
+
+# Import the random module.
+
+# Create the start_game function.
+# Write your code inside this function.
+
+#   When the program starts, we want to:
+#   ------------------------------------
+#   1. Display an intro/welcome message to the player.
+#   2. Store a random number as the answer/solution.
+#   3. Continuously prompt the player for a guess.
+#     a. If the guess is greater than the solution, display to the player "It's lower".
+#     b. If the guess is less than the solution, display to the player "It's higher".
+
+#   4. Once the guess is correct, stop looping, inform the user they "Got it"
+#      and show how many attempts it took them to get the correct number.
+#   5. Let the player know the game is ending, or something that indicates the game is over.
+
+# ( You can add more features/enhancements if you'd like to. )
+
+
+# Kick off the program by calling the start_game function.
